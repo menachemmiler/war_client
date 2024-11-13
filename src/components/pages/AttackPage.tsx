3f912627eb5530b0979cdf8dbdb8854400317911
menachem <AM0556727820@GMAIL.COM>
2024-11-13 17:59:26 +0200

    with table resources

--- src/components/pages/AttackPage.tsx
+++ src/components/pages/AttackPage.tsx
@@ -1,14 +1,35 @@
+import { IAttack } from "../../models/attack";
+import { IResources } from "../../models/organiz";
 import { useAppSelector } from "../../redux/store";
+import Nav from "../Nav";
 
 export default function AttackPage() {
   const { user: user } = useAppSelector((state) => state.user);
 
   return (
-    <div className="userpage">
+    <div className="attackpage">
+      <Nav />
       <h1>AttackPage</h1>
       <h1>{`username: ${user?.username}`}</h1>
       <h2>{`organiz.name: ${user?.organiz.name}`}</h2>
       <h3>{`_id: ${user?._id}`}</h3>
+      {/* table to all resorcees */}
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {user?.organiz.resources.map((resource: IResources) => (
+            <tr key={resource.name}>
+              <td>{resource.name}</td>
+              <td>{resource.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/components/pages/ProtectionPage.tsx
+++ src/components/pages/ProtectionPage.tsx
@@ -1,15 +1,32 @@
+import { IResources } from "../../models/organiz";
 import { useAppSelector } from "../../redux/store";
 
 export default function ProtectionPage() {
   const { user: user } = useAppSelector((state) => state.user);
 
   return (
-    <div className="userpage">
+    <div className="protectionpage">
       <h1>ProtectionPage</h1>
       <h1>{`username: ${user?.username}`}</h1>
       <h2>{`organiz.name: ${user?.organiz.name}`}</h2>
       <h2>{`organiz.area: ${user?.area}`}</h2>
       <h3>{`_id: ${user?._id}`}</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {user?.organiz.resources.map((resource: IResources) => (
+            <tr key={resource.name}>
+              <td>{resource.name}</td>
+              <td>{resource.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
